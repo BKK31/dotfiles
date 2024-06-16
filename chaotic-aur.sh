9d62840f424684866f13b0454507a30f791d2c47
Bhargava K K <98461761+BKK31@users.noreply.github.com>
2024-06-16 17:14:19 +0530

    Update chaotic-aur.sh

--- chaotic-aur.sh
+++ chaotic-aur.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key 3056513887B78AEB
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+wget -q -O chaotic-AUR-installer.bash https://raw.githubusercontent.com/SharafatKarim/chaotic-AUR-installer/main/install.bash && sudo bash chaotic-AUR-installer.bash && rm chaotic-AUR-installer.bash
