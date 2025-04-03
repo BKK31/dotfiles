21a3829d382f075be75cbf8885c13a74b596f68c
Bhargava K K <bhargavakk13@gmail.com>
2025-04-03 18:02:21 +0530

    neovim new config

--- config/nvim/lua/plugins/markview.lua
+++ config/nvim/lua/plugins/markview.lua
@@ -1,6 +1,9 @@
--- For `plugins/markview.lua` users.
 return {
-    "OXY2DEV/markview.nvim",
-    lazy = false,
-    vim.keymap.set('n', '<C-t>', ':Markview Toggle<CR>', { noremap = true, silent = true }) -- focus file explorer
-};
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+        vim.keymap.set('n', '<C-t>', ':RenderMarkdown toggle<CR>', { noremap = true, silent = true })
+    },
+}
